89a7a62c5787f6e547f4fd8717c0260a5afe8faa
Pratik Deshmane <pratik.s.deshmane91@gmail.com>
2019-01-25 17:45:55 +0530

    theme for Real Estate
    Theme for real estate using material bootstrap design

---
+++ 2bhk flats/js/script.js
@@ -0,0 +1,15 @@
+// onscroll logo change 
+window.onscroll = function() {
+  growShrinkLogo()
+};
+
+function growShrinkLogo() {
+  var Logo = document.getElementById("Logo")
+  if (document.body.scrollTop > 3 || document.documentElement.scrollTop < 5) {
+    Logo.style.width = '200px';
+    Logo.src='./img/rera.png';
+  } else {
+    Logo.style.width = '50px';
+    Logo.src='./img/kp.png';
+  }
+}
