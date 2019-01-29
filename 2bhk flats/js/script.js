057b74a86f2137301c9222cef2c218c9a935fa6c
Pratik Deshmane <pratik.s.deshmane91@gmail.com>
2019-01-29 13:04:24 +0530

    speed Optimisation
    converted all images to webp format

--- 2bhk flats/js/script.js
+++ 2bhk flats/js/script.js
@@ -7,14 +7,14 @@ function growShrinkLogo() {
   var Logo = document.getElementById("Logo")
   if (document.body.scrollTop > 3 || document.documentElement.scrollTop < 5) {
     Logo.style.width = '200px';
-    Logo.src='./img/rera.png';
+    Logo.src='./img/rera.webp';
   } else {
     Logo.style.width = '50px';
-    Logo.src='./img/kp.png';
+    Logo.src='./img/kp.webp';
   }
 }
 
 // rera logo
 
-$(".overlay").append("<img id='theImg' src='./img/re.png'/>");
+$(".overlay").append("<img id='theImg' src='./img/re.webp'/>");
 
